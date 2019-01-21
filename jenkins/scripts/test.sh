b894dc4914169b734131d6c73f74292189a8e3b9
unknown88888 <unknown@qqq.kiev.ua>
2019-01-21 11:01:59 +0200

    Updated test.sh

--- jenkins/scripts/test.sh
+++ jenkins/scripts/test.sh
@@ -2,13 +2,9 @@
 
 echo Run QA test script
 
-#!/bin/bash
-
-#sleep $(/usr/bin/shuf -i 5-20 -n 1)
-
-random=$(/usr/bin/shuf -i 0-1 -n 1)
-#[ $random -eq 2 ] && sed -i 's/\(\/h1\)/h1/g' /usr/local/apache2/htdocs/index.html
-#[ $random -eq 3 ] && sed -i 's/\(lang="en"\)//g' /usr/local/apache2/htdocs/index.html
+random=$(/usr/bin/shuf -i 0-3 -n 1)
 
 echo $random
-exit $random
+[ "$random" = "1" ] && exit 1
+
+exit 0
